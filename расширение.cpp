2ee588ada09d5fc0aefd5d2ee3273db612f4bcb2
Гусев Роман Викторович <91760597+gromvig@users.noreply.github.com>
2021-11-27 16:50:39 +0300

    Add files via upload

---
+++ расширение.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+#include<string.h>
+#include<windows.h>
+using namespace std;
+void F(string &a,string b) {
+	int k = 0;
+	int i = a.find_last_of('.');
+	if (i < 0) {
+		a += "." + b;
+		return;
+	}
+	k = a.length() - i - 1;
+	a.replace(i + 1, k, b);
+}
+	int main() {
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		cout << "Введите имя файла для изменения\n";
+		string a;
+		cin >> a;
+		cout << "Введите необходимое расширение\n";
+		string b;
+		cin >> b;
+		F(a, b);
+		cout << a;
+	}
+
---
+++ слово в строке.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+#include<string.h>
+#include<windows.h>
+using namespace std;
+int F(string a, string b) {
+	int c = 0;
+	int k = 0;
+	int d = b.length();
+	while (1) {
+		c = a.find(b, c);
+		if (c < 0) {
+			return(k);
+		}
+		c += d;
+		k++;
+	}
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите строку:\n";
+	string a, b;
+	getline(cin, a);
+	cout << "Введите слово для поиска в строке:\n";
+	cin >> b;
+	cout << "Заданное слово входит в строку " << F(a,b) << " раз";
+	return(0);
+}
